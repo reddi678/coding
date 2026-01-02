6e9360c37e40c6faada266e9035d1aad75ca55a1
Madhuri <reddimadhuri840@gmail.com>
2026-01-02 14:59:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+//replacing alhabet in a string
+
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String r="";
+        for(int i=0;i<s.length();i++)
+        {
+            int x;
+            x=s.charAt(i);
+            if(x>='a' && x<='z')
+            {
+                x=((int)x+1)%123;
+                if(x==0)
+                x=97;
+                char x1=(char)x;
+                if(!(x1=='a'||x1=='e'||x1=='i'||x1=='o'||x1=='u'))
+                r=r+x1;
+            }
+            else{
+                r=r+s.charAt(i);
+            }
+        }
+        System.out.println(r);
+    }
+}
